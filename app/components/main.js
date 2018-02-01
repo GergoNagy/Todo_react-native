94530f2670f9be561c89de6e88159aa0e2415dbb
Gergo Nagy <guzmop@gmail.com>
2018-02-01 14:18:56 +0000

    add the main component

---
+++ app/components/main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    ScrollView,
+    TouchableOpacity
+} from 'react-native';
+
+export default class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    main
+                </Text>
+            </View>
+        );
+    }
+}
